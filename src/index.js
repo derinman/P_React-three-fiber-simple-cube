ce4c088034f6bbc28af0893681634e32ba9bb89a
inman <sf990668852@gmail.com>
2020-07-30 20:52:56 +0800

    改資料夾結構

--- src/index.js
+++ src/index.js
@@ -1,15 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Canvas } from 'react-three-fiber'
-import Box from "../src/App";
+import App from "../src/App";
 import './styles.css'
 
 ReactDOM.render(
-    <Canvas>
-    <ambientLight />
-    <pointLight position={[10, 10, 10]} />
-    <Box position={[-1.2, 0, 0]} />
-    <Box position={[1.2, 0, 0]} />
-    </Canvas>,
+    <App/>,
     document.getElementById('root')
 )
